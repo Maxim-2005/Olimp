e03277a596c57c10615737cac6dbfd8572a0491b
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2022-01-12 21:55:42 +0300

    20.4

---
+++ Конкурс/20.4.py
@@ -0,0 +1,24 @@
+N = int(input())
+arr = [0] * ((N-1) // 2)
+T = int(input())
+for i in range(T):
+    K, S = map(int, input().split())
+    arr[K-1] = arr[K-1] + S % ((N-1) * 4 - ((K - 1) * 8))
+
+matrix = [0]*N
+for i in range(N):
+    matrix[i] = [1]*N
+
+for i in range(len(arr)):
+    tN = N - i * 2
+    L = tN - 1 * 4
+    for x in range(tN - 1):
+         matrix[i][((N - tN) // 2 + x)] = x + 1
+
+    for y in range(N):
+        #лево
+        #право
+        pass
+
+for i in matrix:
+    print(*i)
