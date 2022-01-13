6547e466c5b398f5d95a82353a5a8c16fa4ccaf7
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2022-01-13 21:09:13 +0300

    20.4 FINAL

--- Конкурс/20.4.py
+++ Конкурс/20.4.py
@@ -11,14 +11,14 @@ for i in range(N):
 
 for i in range(len(arr)):
     tN = N - i * 2
-    L = tN - 1 * 4
-    for x in range(tN - 1):
-         matrix[i][((N - tN) // 2 + x)] = x + 1
+    L = (tN - 1) * 4
+    for x in range(tN):
+         matrix[i][i + x] = (x - arr[i]) % L + 1
+         matrix[N - i - 1][i + x] = (L - (tN - 2)  - x - arr[i] - 1) % L + 1
 
-    for y in range(N):
-        #лево
-        #право
-        pass
+    for y in range(tN-2):
+        matrix[y + i + 1][i] = (L - y - arr[i] - 1)% L + 1
+        matrix[y + i + 1][N - i - 1] = (tN + y - arr[i]) % L + 1
 
 for i in matrix:
     print(*i)
