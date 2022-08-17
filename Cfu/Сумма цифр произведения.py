ea8026fe8f47f94d6319e711f6d231b11343b397
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2022-08-17 21:44:37 +0300

    Update Сумма цифр произведения.py

--- Cfu/Сумма цифр произведения.py
+++ Cfu/Сумма цифр произведения.py
@@ -1,12 +1,1 @@
-for i in range(int(input())):
-	n, A = map(int, input().split())
-	s = 0
-	if len(str(A)) >= n - 1:
-		s = sum(map(int, str(int('1' * n) * A)))
-	else:
-		s = sum(map(int, str(int('1' * len(str(A))) * A)))
-		t = int('1' * (len(str(A)) + 2)) * A
-		v = int(str(t)[len(str(t)) // 2])
-		l = n - len(str(A))
-		s += v * l
-	print(s)
+print(sum(map(int, str(int('1' * n)a)))) if len(str(a)) >= n-1 else print(sum(map(int, str(int('1' len(str(a)))a))) + int(str(int('1'(len(str(a))+2))a)[len(str(int('1'(len(str(a))+2))a))//2]) (n - len(str(a))))
