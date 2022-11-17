3772f9f7f56ed78dee9cac7d5f21d836ced51c9d
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2022-11-17 21:35:30 +0300

    Update 5-Самый длинный палиндром.py

--- Town-2022/5-Самый длинный палиндром.py
+++ Town-2022/5-Самый длинный палиндром.py
@@ -1,18 +1,31 @@
 S = '-' + '-'.join(input()) + '-'
-peak = 1
 d = len(S)
+arr = [0] * d
+peak = c = r = 0
 
-for i in range(len(S)):
-	temp =  -1
-	for j in range(min(i, d - i - 1) + 1):
-		if S[i - j] == S[i + j]:
-			temp += 1
-			peak = max(peak, temp)
-		else:
-			break
+for i in range(d):
+	j = 0 if i > c + r  else min(arr[c-(i-c)], c + r - i)
+	while i - j > 0 and i + j < d - 1 and S[i - j - 1] == S[i + j + 1]:
+		j += 1
+	if i + j > c + r:
+		c, r = i, j
+	arr[i] = j
+	peak = max(peak, j)
 print(peak)
 
-
+#S = '-' + '-'.join(input()) + '-'
+#peak = 1
+#d = len(S)
+#
+#for i in range(len(S)):
+#	temp =  -1
+#	for j in range(min(i, d - i - 1) + 1):
+#		if S[i - j] == S[i + j]:
+#			temp += 1
+#			peak = max(peak, temp)
+#		else:
+#			break
+#print(peak)
 
 #S = input()
 #peak = 0
